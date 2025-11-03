e0e9839cc865dc36fbe4d98c6eef90cef6cc6941
Yqnk <yan.bertile@gmail.com>
2025-11-04 00:49:19 +0100

    feat: update to nvchad 2.5 (some breaking changes in NeoVIM forced me to do so, WIP

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,8 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = "jellybeans", transparency = true, tabufline = { enabled = false } }
-M.mappings = require "custom.mappings"
-M.plugins = "custom.plugins"
-
-return M
---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,46 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+local nomap = vim.keymap.del
+
+--general
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<A-j>", "<cmd> m +1 <CR>")
+map("n", "<A-k>", "<cmd> m -2 <CR>")
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- conform (format)
+nomap({ "n", "x" }, "<leader>fm")
+map({ "n", "x" }, "<leader>f", function()
+  require("conform").format { lsp_fallback = true }
+end, { desc = "general format file " })
+
+-- nvimtree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<C-n>", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+
+-- telescope
+map("n", "<leader>sg", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>sh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+map("n", "<leader>sm", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>so", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+map("n", "<leader>sfz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+map("n", "<leader>gcm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+map("n", "<leader>st", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+
+nomap("n", "<leader>fw")
+nomap("n", "<leader>fb")
+nomap("n", "<leader>fh")
+nomap("n", "<leader>ma")
+nomap("n", "<leader>fo")
+nomap("n", "<leader>fz")
+nomap("n", "<leader>cm")
+nomap("n", "<leader>gt")
+nomap("n", "<leader>pt")
