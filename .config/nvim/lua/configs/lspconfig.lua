aee78cb2511fa3d40bf0185a76748a71a30a1dcd
Yqnk <yan.bertile@gmail.com>
2026-02-19 18:46:55 +0100

    ok

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- jdtls enabled through autocmds
-local servers = { "html", "cssls", "rust_analyzer", "clangd", "vtsls", "gopls", "pyright" }
+local servers = { "html", "cssls", "clangd", "vtsls", "gopls", "pyright" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
