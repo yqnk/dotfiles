514927a0d7efe33ecc038b8c35ba3d2dc1f00cac
Yqnk <yan.bertile@gmail.com>
2026-04-30 14:45:26 +0200

    update

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,18 +1,39 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "everblush",
   transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
+  hl_add = {
+    -- WhichKeyBorder = { fg = "#22292b" },
+    -- WhichKeyNormal = { bg = "#141b1e" },
+  },
+}
+
+M.ui = {
+  cmp = {
+    icons_left = true,
+    style = "flat_dark",
+    format_colors = { lsp = true },
+  },
+
+  telescope = {
+    style = "bordered",
+  },
+
+  statusline = {
+    theme = "minimal",
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -16,11 +16,8 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = { 
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      preset = "helix"
+    opts = {
+      preset = "helix",
     },
   },
 
