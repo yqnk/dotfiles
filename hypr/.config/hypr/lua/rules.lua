a677c35a6f2a83c603461b0c9721ac21af921d20
Yqnk <yan.bertile@gmail.com>
2026-09-06 22:01:17 +0200

    blur popups

--- hypr/.config/hypr/lua/rules.lua
+++ hypr/.config/hypr/lua/rules.lua
@@ -1,11 +1,22 @@
 -- Window / layer rules + xwayland.
 
 hl.layer_rule({
-	name  = "blur-apps-bg",
-	match = { namespace = "waybar|rofi|quickshell|qs" },
-	blur  = true,
-	xray  = true,
-    ignore_alpha = 0.5,
+	name = "blur-apps-bg",
+	match = { namespace = "waybar|rofi" },
+	blur = true,
+	xray = true,
+	ignore_alpha = false,
+})
+
+hl.layer_rule({
+	name = "qs-blur",
+	match = {
+		namespace = "^(quickshell|qs).*",
+	},
+	blur = true,
+	xray = true,
+	blur_popups = true,
+	ignore_alpha = 0.3,
 })
 
 hl.config({
@@ -15,21 +26,21 @@ hl.config({
 })
 
 hl.window_rule({
-	name  = "suppress-maximize-events",
+	name = "suppress-maximize-events",
 	match = { class = ".*" },
 
 	suppress_event = "maximize",
 })
 
 hl.window_rule({
-	name  = "fix-xwayland-drags",
+	name = "fix-xwayland-drags",
 	match = {
-		class      = "^$",
-		title      = "^$",
-		xwayland   = true,
-		float      = true,
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
 		fullscreen = false,
-		pin        = false,
+		pin = false,
 	},
 
 	no_focus = true,
