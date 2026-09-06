abd91dd28b6a186172ec66e58c82cdcc847457a5
Yqnk <yan.bertile@gmail.com>
2026-09-06 22:11:19 +0200

    fix: neotree tab key rebind

--- nvim/.config/nvim/lua/configs/neotree.lua
+++ nvim/.config/nvim/lua/configs/neotree.lua
@@ -2,19 +2,9 @@ local M = {
   window = {
     width = 30,
     mappings = {
-      ["<tab>"] = "open",
+      ["<Tab>"] = "open",
       ["<cr>"] = "open",
     },
-  },
-  commands = {
-    tab_open_or_expand = function(state)
-      local node = state.tree:get_node()
-      if node.type == "directory" then
-        require("neo-tree.sources.filesystem.commands").toggle_node(state)
-      else
-        require("neo-tree.sources.filesystem.commands").open(state)
-      end
-    end,
   },
   -- nesting_rules = {},
   filesystem = {
